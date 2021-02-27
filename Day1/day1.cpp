4a1cfeaadaadd440065b2239b5fd79b7bec67757
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2021-02-27 23:20:32 +0530

    Day1 problem improved

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -1,4 +1,5 @@
-// Problem: Rearrange Prank
+// Problem: Rearrange Geek and his Classmates 
+// Refer day1 text file for full problem statement.
 
 //Initial template for C++
 
@@ -18,8 +19,8 @@ class Solution {
         a[i]+=a[(int)a[i]]%n * n;
         for(int i=0;i<n;i++)
         a[i]/=n;
-        }
-    };
+    }
+};
 
 
 // { Main Code
@@ -44,7 +45,13 @@ int main() {
 }
 
 // } End
-// Time complexity: O(n), Space complexity: O(1)
 
-// Similar referance for this problem is::
-// https://www.geeksforgeeks.org/rearrange-given-array-place/
+//Output Win
+/* 
+For Input:
+6
+0 5 1 2 4 3
+
+your output is: 
+0 3 5 1 4 2 
+*/
